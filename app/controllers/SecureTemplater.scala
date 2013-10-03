b157cc759f50946d2ff78673ca1e1d81a57d38d7
Hu Min <minispring1013@gmail.com>
2013-10-03 21:39:17 +0800

    Add css files and plugins

---
+++ app/controllers/SecureTemplater.scala
@@ -0,0 +1,150 @@
+package controllers
+
+import play.api.mvc.{RequestHeader, Request}
+import play.api.templates.{Html, Txt}
+import play.api.{Logger, Plugin, Application}
+import securesocial.core.{Identity, SecuredRequest, SocialUser}
+import play.api.data.Form
+import securesocial.controllers.Registration.RegistrationInfo
+import securesocial.controllers.PasswordChange.ChangeInfo
+import views.secure._
+
+
+class SecureTemplater(application: Application) extends TemplatesPlugin 
+{
+ /**
+   * Returns the html for the login page
+   * @param request
+   * @tparam A
+   * @return
+   */
+  override def getLoginPage[A](implicit request: Request[A], form: Form[(String, String)],
+                               msg: Option[String] = None): Html =
+  {
+    views.secure.html.login(form, msg)
+  }
+
+  /**
+   * Returns the html for the signup page
+   *
+   * @param request
+   * @tparam A
+   * @return
+   */
+  override def getSignUpPage[A](implicit request: Request[A], form: Form[RegistrationInfo], token: String): Html = {
+    views.secure.html.Registration.signUp(form, token)
+  }
+
+  /**
+   * Returns the html for the start signup page
+   *
+   * @param request
+   * @tparam A
+   * @return
+   */
+  override def getStartSignUpPage[A](implicit request: Request[A], form: Form[String]): Html = {
+    views.secure.html.Registration.startSignUp(form)
+  }
+
+  /**
+   * Returns the html for the reset password page
+   *
+   * @param request
+   * @tparam A
+   * @return
+   */
+  override def getStartResetPasswordPage[A](implicit request: Request[A], form: Form[String]): Html = {
+    views.secure.html.Registration.startResetPassword(form)
+  }
+
+  /**
+   * Returns the html for the start reset page
+   *
+   * @param request
+   * @tparam A
+   * @return
+   */
+  def getResetPasswordPage[A](implicit request: Request[A], form: Form[(String, String)], token: String): Html = {
+    views.secure.html.Registration.resetPasswordPage(form, token)
+  }
+
+   /**
+   * Returns the html for the change password page
+   *
+   * @param request
+   * @param form
+   * @tparam A
+   * @return
+   */
+  def getPasswordChangePage[A](implicit request: SecuredRequest[A], form: Form[ChangeInfo]): Html = {
+    views.secure.html.passwordChange(form)      
+  }
+
+
+  /**
+   * Returns the email sent when a user starts the sign up process
+   *
+   * @param token the token used to identify the request
+   * @param request the current http request
+   * @return a String with the html code for the email
+   */
+  def getSignUpEmail(token: String)(implicit request: RequestHeader): String = {
+    views.secure.html.mails.signUpEmail(token).body
+  }
+
+  /**
+   * Returns the email sent when the user is already registered
+   *
+   * @param user the user
+   * @param request the current request
+   * @return a String with the html code for the email
+   */
+  def getAlreadyRegisteredEmail(user: SocialUser)(implicit request: RequestHeader): String = {
+    views.secure.html.mails.alreadyRegisteredEmail(user).body
+  }
+
+  /**
+   * Returns the welcome email sent when the user finished the sign up process
+   *
+   * @param user the user
+   * @param request the current request
+   * @return a String with the html code for the email
+   */
+  def getWelcomeEmail(user: SocialUser)(implicit request: RequestHeader): String = {
+    views.secure.html.mails.welcomeEmail(user).body
+  }
+
+  /**
+   * Returns the email sent when a user tries to reset the password but there is no account for
+   * that email address in the system
+   *
+   * @param request the current request
+   * @return a String with the html code for the email
+   */
+  def getUnknownEmailNotice()(implicit request: RequestHeader): String = {
+    views.secure.html.mails.unknownEmailNotice(request).body
+  }
+
+  /**
+   * Returns the email sent to the user to reset the password
+   *
+   * @param user the user
+   * @param token the token used to identify the request
+   * @param request the current http request
+   * @return a String with the html code for the email
+   */
+  def getSendPasswordResetEmail(user: SocialUser, token: String)(implicit request: RequestHeader): String = {
+    views.secure.html.mails.passwordResetEmail(user, token).body
+  }
+
+  /**
+   * Returns the email sent as a confirmation of a password change
+   *
+   * @param user the user
+   * @param request the current http request
+   * @return a String with the html code for the email
+   */
+  def getPasswordChangedNoticeEmail(user: SocialUser)(implicit request: RequestHeader): String = {
+    views.secure.html.mails.passwordChangedNotice(user).body
+  }
+}
--- project/Build.scala
+++ project/Build.scala
@@ -9,13 +9,18 @@ object ApplicationBuild extends Build {
 
   val appDependencies = Seq(
     // Add your project dependencies here,
-    jdbc,
-    anorm
+	"securesocial" %% "securesocial" % "master-SNAPSHOT",
+	"org.reactivemongo" %% "play2-reactivemongo" % "0.10-SNAPSHOT",
+	"com.feth" %% "play-easymail" % "0.3-SNAPSHOT"
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here      
+	resolvers += Resolver.url("sbt-plugin-snapshots", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns),
+	resolvers += "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
+	resolvers += Resolver.url("play-easymail (release)", url("http://joscha.github.com/play-easymail/repo/releases/"))(Resolver.ivyStylePatterns),
+	resolvers += Resolver.url("play-easymail (snapshot)", url("http://joscha.github.com/play-easymail/repo/snapshots/"))(Resolver.ivyStylePatterns)
   )
 
 }
