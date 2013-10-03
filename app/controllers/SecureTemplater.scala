7c1a0b6177e809296a6587afd8261eff457bd84a
Hu Min <minispring1013@gmail.com>
2013-10-03 23:20:14 +0800

    add navbar

--- app/controllers/SecureTemplater.scala
+++ app/controllers/SecureTemplater.scala
@@ -2,149 +2,68 @@ package controllers
 
 import play.api.mvc.{RequestHeader, Request}
 import play.api.templates.{Html, Txt}
-import play.api.{Logger, Plugin, Application}
+import play.api.{Logger, Plugin}
 import securesocial.core.{Identity, SecuredRequest, SocialUser}
 import play.api.data.Form
 import securesocial.controllers.Registration.RegistrationInfo
 import securesocial.controllers.PasswordChange.ChangeInfo
-import views.secure._
+import securesocial.controllers.TemplatesPlugin
+import views._
 
 
 class SecureTemplater(application: Application) extends TemplatesPlugin 
 {
- /**
-   * Returns the html for the login page
-   * @param request
-   * @tparam A
-   * @return
-   */
-  override def getLoginPage[A](implicit request: Request[A], form: Form[(String, String)],
-                               msg: Option[String] = None): Html =
-  {
-    views.secure.html.login(form, msg)
-  }
+	override def getLoginPage[A](implicit request: Request[A], form: Form[(String, String)],
+	                               msg: Option[String] = None): Html =
+	  {
+	    html.secure.login(form, msg)
+	  }
 
-  /**
-   * Returns the html for the signup page
-   *
-   * @param request
-   * @tparam A
-   * @return
-   */
-  override def getSignUpPage[A](implicit request: Request[A], form: Form[RegistrationInfo], token: String): Html = {
-    views.secure.html.Registration.signUp(form, token)
-  }
+	  override def getSignUpPage[A](implicit request: Request[A], form: Form[RegistrationInfo], token: String): Html = {
+	    html.secure.Registration.signUp(form, token)
+	  }
 
-  /**
-   * Returns the html for the start signup page
-   *
-   * @param request
-   * @tparam A
-   * @return
-   */
-  override def getStartSignUpPage[A](implicit request: Request[A], form: Form[String]): Html = {
-    views.secure.html.Registration.startSignUp(form)
-  }
+	  override def getStartSignUpPage[A](implicit request: Request[A], form: Form[String]): Html = {
+	    html.secure.Registration.startSignUp(form)
+	  }
 
-  /**
-   * Returns the html for the reset password page
-   *
-   * @param request
-   * @tparam A
-   * @return
-   */
-  override def getStartResetPasswordPage[A](implicit request: Request[A], form: Form[String]): Html = {
-    views.secure.html.Registration.startResetPassword(form)
-  }
+	  override def getStartResetPasswordPage[A](implicit request: Request[A], form: Form[String]): Html = {
+	    html.secure.Registration.startResetPassword(form)
+	  }
 
-  /**
-   * Returns the html for the start reset page
-   *
-   * @param request
-   * @tparam A
-   * @return
-   */
-  def getResetPasswordPage[A](implicit request: Request[A], form: Form[(String, String)], token: String): Html = {
-    views.secure.html.Registration.resetPasswordPage(form, token)
-  }
+	  def getResetPasswordPage[A](implicit request: Request[A], form: Form[(String, String)], token: String): Html = {
+	    html.secure.Registration.resetPasswordPage(form, token)
+	  }
 
-   /**
-   * Returns the html for the change password page
-   *
-   * @param request
-   * @param form
-   * @tparam A
-   * @return
-   */
-  def getPasswordChangePage[A](implicit request: SecuredRequest[A], form: Form[ChangeInfo]): Html = {
-    views.secure.html.passwordChange(form)      
-  }
+	  def getPasswordChangePage[A](implicit request: SecuredRequest[A], form: Form[ChangeInfo]):Html = {
+	    html.secure.passwordChange(form)
+	  }
 
+	  def getNotAuthorizedPage[A](implicit request: Request[A]): Html = {
+	    html.secure.notAuthorized()
+	  }
 
-  /**
-   * Returns the email sent when a user starts the sign up process
-   *
-   * @param token the token used to identify the request
-   * @param request the current http request
-   * @return a String with the html code for the email
-   */
-  def getSignUpEmail(token: String)(implicit request: RequestHeader): String = {
-    views.secure.html.mails.signUpEmail(token).body
-  }
+	  def getSignUpEmail(token: String)(implicit request: RequestHeader): (Option[Txt], Option[Html]) = {
+	    (None, Some(html.secure.mails.signUpEmail(token)))
+	  }
 
-  /**
-   * Returns the email sent when the user is already registered
-   *
-   * @param user the user
-   * @param request the current request
-   * @return a String with the html code for the email
-   */
-  def getAlreadyRegisteredEmail(user: SocialUser)(implicit request: RequestHeader): String = {
-    views.secure.html.mails.alreadyRegisteredEmail(user).body
-  }
+	  def getAlreadyRegisteredEmail(user: Identity)(implicit request: RequestHeader): (Option[Txt], Option[Html]) = {
+	    (None, Some(html.secure.mails.alreadyRegisteredEmail(user)))
+	  }
 
-  /**
-   * Returns the welcome email sent when the user finished the sign up process
-   *
-   * @param user the user
-   * @param request the current request
-   * @return a String with the html code for the email
-   */
-  def getWelcomeEmail(user: SocialUser)(implicit request: RequestHeader): String = {
-    views.secure.html.mails.welcomeEmail(user).body
-  }
+	  def getWelcomeEmail(user: Identity)(implicit request: RequestHeader): (Option[Txt], Option[Html]) = {
+	    (None, Some(html.secure.mails.welcomeEmail(user)))
+	  }
 
-  /**
-   * Returns the email sent when a user tries to reset the password but there is no account for
-   * that email address in the system
-   *
-   * @param request the current request
-   * @return a String with the html code for the email
-   */
-  def getUnknownEmailNotice()(implicit request: RequestHeader): String = {
-    views.secure.html.mails.unknownEmailNotice(request).body
-  }
+	  def getUnknownEmailNotice()(implicit request: RequestHeader): (Option[Txt], Option[Html]) = {
+	    (None, Some(html.secure.mails.unknownEmailNotice(request)))
+	  }
 
-  /**
-   * Returns the email sent to the user to reset the password
-   *
-   * @param user the user
-   * @param token the token used to identify the request
-   * @param request the current http request
-   * @return a String with the html code for the email
-   */
-  def getSendPasswordResetEmail(user: SocialUser, token: String)(implicit request: RequestHeader): String = {
-    views.secure.html.mails.passwordResetEmail(user, token).body
-  }
+	  def getSendPasswordResetEmail(user: Identity, token: String)(implicit request: RequestHeader): (Option[Txt], Option[Html]) = {
+	    (None, Some(html.secure.mails.passwordResetEmail(user, token)))
+	  }
 
-  /**
-   * Returns the email sent as a confirmation of a password change
-   *
-   * @param user the user
-   * @param request the current http request
-   * @return a String with the html code for the email
-   */
-  def getPasswordChangedNoticeEmail(user: SocialUser)(implicit request: RequestHeader): String = {
-    views.secure.html.mails.passwordChangedNotice(user).body
-  }
+	  def getPasswordChangedNoticeEmail(user: Identity)(implicit request: RequestHeader): (Option[Txt], Option[Html]) = {
+	    (None, Some(html.secure.mails.passwordChangedNotice(user)))
+	  }
 }
